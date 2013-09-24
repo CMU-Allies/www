4d7db4cc00c246544e533bbd38f252225af58080
Edward <tedgarb2@gmail.com>
2013-09-24 12:51:50 -0400

    Fix t shirts

--- app/models/tshirt.rb
+++ app/models/tshirt.rb
@@ -1,2 +1,8 @@
 class Tshirt < ActiveRecord::Base
+  validates_presence_of :name, :size, :price
+  validates_numericality_of :price
+
+  def self.available_shirts
+    Tshirt.where(:available => true)
+  end
 end
