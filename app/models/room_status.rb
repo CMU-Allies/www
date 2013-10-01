df674d5f8ea4428dc99d3534243760fdae8b1dca
allies <allies@allies-03.andrew.cmu.edu>
2013-10-01 17:37:25 -0400

    I really hope this works

--- app/models/room_status.rb
+++ app/models/room_status.rb
@@ -4,7 +4,7 @@ class RoomStatus < ActiveRecord::Base
   validates :user, presence: true
 
   # status of true means room is open, status of false means room is closed
-  def self.latest
+  def latest
     self.last(:order => :created_at)
   end
 
