3a98d30375e38bac4afcc1141e42f7f7774b333e
Feffernoose <fefferburbia@gmail.com>
2013-11-09 17:27:28 -0500

    Added mailer.rb initializer for production emailing

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,6 +32,7 @@ namespace :deploy do
     run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/secret_token.rb #{ release_path }/config/initializers/secret_token.rb"
     run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/devise.rb #{ release_path }/config/initializers/devise.rb"
     run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/google_drive.yml #{ release_path }/config/google_drive.yml"
+    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/mailer.rb #{ release_path }/config/initializers/mailer.rb"
   end
 end
 
