ccced4f86f124c7881cdf21993c90ef73e198313
Feffernoose <fefferburbia@gmail.com>
2013-11-14 19:36:03 -0500

    Fixed singletonize migration

--- db/migrate/20131114234237_singletonize_room_status.rb
+++ db/migrate/20131114234237_singletonize_room_status.rb
@@ -1,6 +1,7 @@
 class SingletonizeRoomStatus < ActiveRecord::Migration
-  def change
-    execute "DELETE FROM room_statuses WHERE id < (SELECT id FROM room_statuses ORDER BY created_at DESC LIMIT 1)"
+  def up
+    l = execute "SELECT id FROM room_statuses ORDER BY created_at DESC LIMIT 1"
+    execute "DELETE FROM room_statuses WHERE id < " + l[0][0].to_s
     change_column :room_statuses, :user_id, :integer, :null => true, :default => nil
   end
   
