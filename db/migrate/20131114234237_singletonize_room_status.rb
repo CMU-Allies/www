0e3c3ebd5cd0477164d58e1d009cb0098fab9d43
Feffernoose <fefferburbia@gmail.com>
2013-11-14 19:44:47 -0500

    Fixed singletonize migration AGAIN

--- db/migrate/20131114234237_singletonize_room_status.rb
+++ db/migrate/20131114234237_singletonize_room_status.rb
@@ -1,7 +1,6 @@
 class SingletonizeRoomStatus < ActiveRecord::Migration
   def up
-    l = execute "SELECT id FROM room_statuses ORDER BY created_at DESC LIMIT 1"
-    execute "DELETE FROM room_statuses WHERE id < " + l[0][0].to_s
+    execute "DELETE FROM room_statuses WHERE id < " + RoomStatus.__send__(:maximum, :id).to_s
     change_column :room_statuses, :user_id, :integer, :null => true, :default => nil
   end
   
