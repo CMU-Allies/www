afbad141eb2abadad22bb94a4c8bd0af08dc2470
Colin Meret <colinmeret@gmail.com>
2014-04-03 17:38:54 -0400

    cleaned up myjquery.js code and application.css.scss code

--- app/assets/javascripts/myjquery.js
+++ app/assets/javascripts/myjquery.js
@@ -1,13 +1,4 @@
 $(document).ready( function() {
-/*
-  $("#About").hide()
-  $("#Projects").hide()
-  $("#Resume").hide()
-  $("#Links").hide()
-  $("#Contact").hide()
-  $(".Resume").hide()
-*/
-
   function fullalpha(item){
     var hxcolor = $(item).css("background-color")
     var red = hxcolor.substring(5,8)
@@ -24,29 +15,6 @@ $(document).ready( function() {
     var nrgba = "rgba(" + red + ", " + blu + ", " + gre + ",.5)"
     return $(item).css("background-color", nrgba)
   }
-
-/*  var selectTab = $("div#Home")
-  function tabSelect(item) {
-    var newSelect = $(item).text()
-    selectTab.stop().slideToggle()
-    selectTab = $("div#" + newSelect)
-    selectTab.stop().css('z-index', '1').fadeToggle('slow')
-  }
-*/
-  /*
-  $(window).scroll(function() {
-    if($("body").scrollTop() > 240) {
-      $(".topnavbar").css({"position" : "fixed", "left" : "0%", "background" : "white", "height" : "40px", "width" : "100%"})
-      $(".topnavbar").children().css({"margin-top" : "0px"})
-    }
-    if($("body").scrollTop() < 240) {
-      $(".topnavbar").css({"position" : "static", "margin-left" : "auto", "background" : "none"})
-      $(".topnavbar").children().css({"margin-top" : "0px"})
-    }
-  })
-  */
-//  $("div#dropdown").hide()
-  $(".morecontent").hide()
   $("div.topnav").mouseover( function(){
     brightalpha(this)
     $(this).animate({
@@ -68,18 +36,10 @@ $(document).ready( function() {
     }, {duration: "fast", queue: false}, "linear")
   })
   
-  /* $("#d3").mouseover( function(){
-    $("#dropdown").slideDown(500)
-    $(this).animate({
-      width:'120px',
-      height: '40px',
-      marginTop: '0px'
-    }, "fast")
-  })
-  */
   $(".wrapper").mouseleave( function(){
     $("#dropdown").stop().slideUp(100)
   })
+ 
   $(".dropelem").mouseover( function(){
     brightalpha(this)
     $(this).mousedown( function (){
@@ -89,6 +49,7 @@ $(document).ready( function() {
       })
     })
   })
+  
   $(".topnav").mouseover( function(){
     brightalpha(this)
     $(this).mousedown( function (){
@@ -100,7 +61,8 @@ $(document).ready( function() {
       })
     })
   })
-  $(".dropelem").mouseleave( function(){
+  
+  /*$(".dropelem").mouseleave( function(){
     fullalpha(this)
   })
   
@@ -117,42 +79,8 @@ $(document).ready( function() {
     $("#showless").text("Show more").attr("id", "showmore")
     
   })
-  
-  //$("canvas#drawhead").drawArc({
-  //  fillStyle: "red",
-  //  x: 300, y: 200,
-  //  radius: 30,
-  //  // start and end angles in degrees
-  //}).drawArc({
-  //  fillStyle: "blue",
-  //  x: 100, y: 50,
-  //  radius: 30,
-  //  // start and end angles in degrees
-  //}).drawArc({
-  //  fillStyle: "orange",
-  //  x: 500, y: 150,
-  //  radius: 30,
-  //  // start and end angles in degrees
-  //}).drawArc({
-  //  fillStyle: "purple",
-  //  x: 30, y: 160,
-  //  radius: 30,
-  //  // start and end angles in degrees
-  //}).drawArc({
-  //  fillStyle: "green",
-  //  x: 670, y: 100,
-  //  radius: 30,
-  //  // start and end angles in degrees
-  //}).drawText({
-  //  fillStyle: "black",
-  //  x: 350,
-  //  y: 120,
-  //  align: "center",
-  //  fontSize: 90,
-  //  fontFamily: "Galindo, Viga, Cabin, sans-serif",
-  //  fontStyle: "bold",
-  //  text: "Colin Meret" 
-  //})
+  */
+
   $(".resumeButton").mouseleave( function() {
     fullalpha(this)
     $(this).stop().animate({
@@ -160,20 +88,5 @@ $(document).ready( function() {
     height: '30px',
     fontSize: '17px'
   }, 200)
-  })
-  $(".resumeButton").mouseover( function(){
-    brightalpha(this)
-    $(this).stop().animate({
-      width:'240px',
-      height: '40px',
-      fontSize: '22px'
-    }, 200)
-  })
-  $(".resumeButton").mouseup( function (){
-    brightalpha(this)
-  })
-  $(".resumeButton").mousedown( function (){
-    fullalpha(this)
-    $(this).next().next().stop().slideToggle(200)
   })
 })
