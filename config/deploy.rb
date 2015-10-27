62b8e900d9983ced1c7ef2f89f8503f4684ffc4a
Kelly Rauchenberger <fefferburbia@gmail.com>
2015-10-27 12:32:08 -0400

    Updated deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,26 @@
 require "bundler/capistrano"
+require "rvm/capistrano"
 
-set :application, "ALLIES@CMU"
-set :repository, "https://github.com/CMU-Allies/www"
+set :application, "alliesweb"
+set :domain, "allies.andrew.cmu.edu"
+set :repository,  "https://github.com/CMU-Allies/www.git"
+set :branch, "master"
+set :deploy_to, "/srv/rails/alliesweb"
+set :user, ''
+set :rails_env, "production"
+set :rvm_type, :system
+set :rvm_bin_path, '/usr/local/rvm/bin'
+set :rvm_ruby_string, "2.2.1"
+set :rvm_autolibs_flag, 'packages'
+set :rvm_install_type, :head
+default_run_options[:pty] = true
 
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-server "allies-03.andrew.cmu.edu", :web, :app, :db, :primary => true
-set :user, "allies"
-set :keep_releases, 5
-set :rails_env, "production"
-set :deploy_to, "/srv/rails/www"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, domain                          # Your HTTP server, Apache/etc
+role :app, domain                          # This may be the same as your `Web` server
+role :db,  domain, :primary => true # This is where Rails migrations will run
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
@@ -29,22 +33,17 @@ namespace :deploy do
   desc "Symlink shared config files"
   task :symlink_config_files do
     run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ release_path }/config/database.yml"
-    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/secret_token.rb #{ release_path }/config/initializers/secret_token.rb"
+    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/secrets.yml #{ release_path }/config/secrets.yml"
     run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/devise.rb #{ release_path }/config/initializers/devise.rb"
-    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/google_drive.yml #{ release_path }/config/google_drive.yml"
-    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/mailer.rb #{ release_path }/config/initializers/mailer.rb"
+    run "#{ try_sudo } ln -s #{ deploy_to }/shared/config/pi.yml #{ release_path }/config/pi.yml"
   end
 end
 
 after "deploy:finalize_update", "deploy:symlink_config_files"
-after "deploy", "deploy:restart"
-after "deploy", "deploy:cleanup"
 
 set :whenever_command, "bundle exec whenever"
 require "whenever/capistrano"
 
-# RVM
-require "rvm/capistrano"
-set :rvm_ruby_string, :local        # use the same ruby as used locally for deployment
-before 'deploy:setup', 'rvm:install_rvm'  # install/update RVM
-before 'deploy:setup', 'rvm:install_ruby'
+require "rvm/capistrano/alias_and_wrapp"
+before 'deploy', 'rvm:create_alias'
+before 'deploy', 'rvm:create_wrappers'
