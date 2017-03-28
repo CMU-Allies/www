6c2fbefaac8c9ed9bf371b41099370aaf504b959
Kelly Rauchenberger <fefferburbia@gmail.com>
2017-03-28 15:44:07 -0400

    Fixed Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,8 @@ class ApplicationController < ActionController::Base
   
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :email
-      devise_parameter_sanitizer.for(:sign_up) << :first_name
-      devise_parameter_sanitizer.for(:sign_up) << :last_name
-      devise_parameter_sanitizer.for(:account_update) << :email
-      devise_parameter_sanitizer.for(:account_update) << :first_name
-      devise_parameter_sanitizer.for(:account_update) << :last_name
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:email, :first_name, :last_name])
     end
     
     def room_status
