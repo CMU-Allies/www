7b51a531aba083429308191eb1a1f0312c9a2ed6
Kelly Rauchenberger <fefferburbia@gmail.com>
2017-10-03 18:54:11 -0400

    Converted database tables to utf8mb4
    See ABTech/abtt@fd13b615cafec263d75ea3de90c974454325a160.

---
+++ db/migrate/20171003224537_convert_tables_to_real_unicode.rb
@@ -0,0 +1,83 @@
+class ConvertTablesToRealUnicode < ActiveRecord::Migration[5.0]
+  def up
+    change_column :pages, :slug, :string, limit: 191, null: false
+    change_column :updates, :slug, :string, limit: 191, null: false
+    change_column :users, :email, :string, limit: 191, null: false
+    change_column :users, :login, :string, limit: 191, null: false
+    change_column :users, :reset_password_token, :string, limit: 191
+
+    execute "ALTER DATABASE " + ActiveRecord::Base.connection.current_database + " CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+
+    models = [Commission, ExecBoard, OfficeHourLog, Officer, Page, RoomStatus, Update, User]
+    models.each do |m|
+      tn = m.table_name
+      execute "ALTER TABLE " + tn + " CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+
+      m.columns.each do |c|
+        if /(text|varchar\([0-9]+\))/.match(c.sql_type)
+          constraints = ""
+          if !c.null
+            constraints += " NOT NULL"
+          end
+          if !c.default.nil?
+            constraints += " DEFAULT '" + c.default + "'"
+          end
+          execute "ALTER TABLE " + tn + " MODIFY " + c.name + " " + c.sql_type + " CHARACTER SET utf8mb4 COLLATE utf8mb4_bin"  + constraints + ";"
+        end
+      end
+    end
+
+    execute "ALTER TABLE schema_migrations CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+    execute "ALTER TABLE schema_migrations MODIFY version varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+
+    execute "ALTER TABLE ar_internal_metadata CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+    execute "ALTER TABLE ar_internal_metadata MODIFY `key` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+    execute "ALTER TABLE ar_internal_metadata MODIFY value varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+
+    ["commissions", "ar_internal_metadata", "exec_boards", "office_hour_logs", "officers", "pages", "room_statuses", "updates", "users", "schema_migrations"].each do |table|
+      execute "REPAIR TABLE " + table + ";"
+      execute "OPTIMIZE TABLE " + table + ";"
+    end
+  end
+
+  def down
+    execute "ALTER DATABASE " + ActiveRecord::Base.connection.current_database + " CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+
+    models = [Commission, ExecBoard, OfficeHourLog, Officer, Page, RoomStatus, Update, User]
+    models.each do |m|
+      tn = m.table_name
+      execute "ALTER TABLE " + tn + " CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+
+      m.columns.each do |c|
+        if /(text|varchar\([0-9]+\))/.match(c.sql_type)
+          constraints = ""
+          if !c.null
+            constraints += " NOT NULL"
+          end
+          if !c.default.nil?
+            constraints += " DEFAULT '" + c.default + "'"
+          end
+          execute "ALTER TABLE " + tn + " MODIFY " + c.name + " " + c.sql_type + " CHARACTER SET utf8 COLLATE utf8_unicode_ci" + constraints + ";"
+        end
+      end
+    end
+
+    execute "ALTER TABLE schema_migrations CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+    execute "ALTER TABLE schema_migrations MODIFY version varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+
+    execute "ALTER TABLE ar_internal_metadata CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    execute "ALTER TABLE ar_internal_metadata MODIFY `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci;"
+    execute "ALTER TABLE ar_internal_metadata MODIFY value varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci;"
+
+    change_column :pages, :slug, :string, limit: 255, null: false
+    change_column :updates, :slug, :string, limit: 255, null: false
+    change_column :users, :email, :string, limit: 255, null: false
+    change_column :users, :login, :string, limit: 255, null: false
+    change_column :users, :reset_password_token, :string, limit: 255
+
+    ["commissions", "ar_internal_metadata", "exec_boards", "office_hour_logs", "officers", "pages", "room_statuses", "updates", "users", "schema_migrations"].each do |table|
+      execute "REPAIR TABLE " + table + ";"
+      execute "OPTIMIZE TABLE " + table + ";"
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,127 +10,85 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170516172238) do
+ActiveRecord::Schema.define(version: 20171003224537) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "commissions", force: :cascade do |t|
-    t.text     "description",        limit: 65535
-    t.decimal  "price",                            precision: 10, default: 0, null: false
-    t.datetime "created_at",                                                  null: false
-    t.datetime "updated_at",                                                  null: false
+  create_table "commissions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+    t.text     "description",        limit: 16777215
+    t.decimal  "price",                               precision: 10, default: 0, null: false
+    t.datetime "created_at",                                                     null: false
+    t.datetime "updated_at",                                                     null: false
     t.string   "image_file_name",    limit: 255
     t.string   "image_content_type", limit: 255
-    t.integer  "image_file_size",    limit: 4
+    t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
 
-  create_table "exec_boards", force: :cascade do |t|
+  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "title",      limit: 255
+    t.datetime "at"
+    t.string   "location",   limit: 255
+    t.text     "details",    limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  create_table "exec_boards", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
     t.integer  "year",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "news_feeds", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.string   "url",        null: false
+  create_table "office_hour_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+    t.integer  "user_id"
+    t.boolean  "is_open"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["user_id"], name: "index_office_hour_logs_on_user_id", using: :btree
   end
 
-  create_table "office_hour_logs", force: :cascade do |t|
-    t.integer  "user_id",    limit: 4
-    t.boolean  "is_open",    limit: 1
+  create_table "officers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+    t.string   "name",          limit: 255, null: false
+    t.string   "position",      limit: 255, null: false
+    t.integer  "exec_board_id",             null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["exec_board_id"], name: "index_officers_on_exec_board_id", using: :btree
+  end
+
+  create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+    t.string   "title",      limit: 255,      null: false
+    t.string   "slug",                        null: false
+    t.text     "body",       limit: 16777215
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["user_id"], name: "index_office_hour_logs_on_user_id"
+    t.index ["slug"], name: "index_pages_on_slug", unique: true, using: :btree
   end
 
-  create_table "office_hours", force: :cascade do |t|
-    t.integer  "user_id",    null: false
-    t.string   "day",        null: false
-    t.integer  "starts",     null: false
-    t.integer  "ends",       null: false
+  create_table "room_statuses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+    t.integer  "user_id"
+    t.boolean  "open",              default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["user_id"], name: "index_office_hours_on_user_id"
+    t.boolean  "close_at_midnight", default: true,  null: false
   end
 
-  create_table "officers", force: :cascade do |t|
-    t.string   "name",          null: false
-    t.string   "position",      null: false
-    t.integer  "exec_board_id", null: false
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["exec_board_id"], name: "index_officers_on_exec_board_id"
-  end
-
-  create_table "pages", force: :cascade do |t|
-    t.string   "title",      limit: 255,   null: false
-    t.string   "slug",       limit: 255,   null: false
-    t.text     "body",       limit: 65535
+  create_table "updates", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+    t.string   "title",      limit: 255,      null: false
+    t.string   "slug",                        null: false
+    t.text     "body",       limit: 16777215
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["slug"], name: "index_pages_on_slug", unique: true
+    t.index ["slug"], name: "index_updates_on_slug", unique: true, using: :btree
   end
 
-  create_table "rails_admin_histories", force: :cascade do |t|
-    t.text     "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      limit: 2
-    t.integer  "year",       limit: 5
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["item", "table", "month", "year"], name: "index_rails_admin_histories"
-  end
-
-  create_table "resource_categories", force: :cascade do |t|
-    t.string   "title",      null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "resources", force: :cascade do |t|
-    t.string   "title",                null: false
-    t.text     "text",                 null: false
-    t.string   "url"
-    t.integer  "resource_category_id", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "room_statuses", force: :cascade do |t|
-    t.integer  "user_id",           limit: 4
-    t.boolean  "open",              limit: 1, default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "close_at_midnight", limit: 1, default: true,  null: false
-  end
-
-  create_table "updates", force: :cascade do |t|
-    t.string   "title",      limit: 255,   null: false
-    t.string   "slug",       limit: 255,   null: false
-    t.text     "body",       limit: 65535
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["slug"], name: "index_updates_on_slug", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "login",                  limit: 255,                      null: false
-    t.string   "email",                  limit: 255, default: "",         null: false
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
+    t.string   "login",                                                   null: false
+    t.string   "email",                              default: "",         null: false
     t.string   "encrypted_password",     limit: 255, default: "",         null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,          null: false
+    t.integer  "sign_in_count",                      default: 0,          null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
@@ -141,9 +99,9 @@ ActiveRecord::Schema.define(version: 20170516172238) do
     t.datetime "updated_at"
     t.string   "password_salt",          limit: 255
     t.string   "level",                  limit: 255, default: "inactive", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["login"], name: "index_users_on_login", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["login"], name: "index_users_on_login", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
