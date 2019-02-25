f60f8d5e62578ac82fef453fcb64538020dbd8c1
Kelly Rauchenberger <fefferburbia@gmail.com>
2019-02-25 18:21:21 -0500

    SoHo box displays last updated in Eastern Time

--- app/models/room_status.rb
+++ app/models/room_status.rb
@@ -1,12 +1,16 @@
 class RoomStatus < ApplicationRecord
   acts_as_singleton
   belongs_to :user
-  
+
   def status
     if self.open
       :open
     else
       :closed
     end
+  end
+
+  def display_time
+    updated_at.in_time_zone("Eastern Time (US & Canada)").strftime("%a, %b %d %H:%M")
   end
 end
